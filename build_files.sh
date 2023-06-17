1a2e442c3d14041394a776b1d386f8d3e988a9b8
Yousef <yousefghazal2412@gmail.com>
2023-06-17 11:15:01 +0300

    change color

--- build_files.sh
+++ build_files.sh
@@ -10,4 +10,3 @@ python3 manage.py migrate --noinput
 echo "Collect Static..."
 python3 manage.py collectstatic --noinput --clear
 
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(username='admin', email='admin@example.com', password='1234')" | python manage.py shell
