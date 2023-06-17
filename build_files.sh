ef3d4197acc8303dba5305247b18231de00168a7
Yousef <yousefghazal2412@gmail.com>
2023-06-17 15:43:56 +0300

    create superuser

--- build_files.sh
+++ build_files.sh
@@ -10,3 +10,5 @@ python3 manage.py migrate --noinput
 echo "Collect Static..."
 python3 manage.py collectstatic --noinput --clear
 
+export DJANGO_SUPERUSER_PASSWORD='1234'
+python3 manage.py createsuperuser --noinput --username admin --email 'admin@gmail.com'
