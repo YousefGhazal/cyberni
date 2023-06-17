f46a058b68448b9ffc936efe5c2dadd9b73f6b3d
Yousef <yousefghazal2412@gmail.com>
2023-06-17 08:36:17 +0300

    media root

--- cyberni/settings.py
+++ cyberni/settings.py
@@ -18,6 +18,8 @@ import environ
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
 env.read_env(env.str(str(BASE_DIR), ".env"))
+from os.path import join
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -115,14 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = env("STATIC_URL")
-STATICFILES_DIRS = os.path.join(BASE_DIR, "static"),
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", 'static')
+STATIC_ROOT = join(BASE_DIR, "staticfiles_build", "static")
 
 
 MEDIA_URL = env("MEDIA_URL")
-MEDIA_ROOT = env("MEDIA_ROOT")
-
-
+MEDIA_ROOT = join(BASE_DIR, "staticfiles_build", "media")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
